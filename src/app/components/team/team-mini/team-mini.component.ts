5e03b1cc7254586fc4c9298aa8bf30caed985570
Bouchaib <bouchaibfaham@gmail.com>
2021-05-18 21:35:00 +0200

    feat(teamselect): modification de la page teamSelect

--- src/app/components/team/team-mini/team-mini.component.ts
+++ src/app/components/team/team-mini/team-mini.component.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-// ajout 18 mai début
-import * as team from '../../../images/_remove/teams/1.png';
-// ajout 18 mai fin
 @Component({
   selector: 'app-team-mini',
   templateUrl: './team-mini.component.html',
@@ -9,7 +6,6 @@ import * as team from '../../../images/_remove/teams/1.png';
 })
 export class TeamMiniComponent implements OnInit {
 
-  bonjour = 32;
   constructor() { }
 
   ngOnInit(): void {
@@ -18,11 +14,3 @@ export class TeamMiniComponent implements OnInit {
 }
 
 
-front\src\assets\images\_remove\teams
-
-
-
-declare module '*.jpg' {
-    const value: any;
-    export default value;
- }
