7c1013c68fe16c68db03bb1f675cc245feb17927
Jason Vennin <jason.vennin@gmail.com>
2021-05-28 18:05:02 +0200

    feat(team-emoji): add ts html, not completed

--- src/app/pages/params/team/team-emojis/team-emojis.component.ts
+++ src/app/pages/params/team/team-emojis/team-emojis.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { EmojiNamePict } from 'src/app/features/messages/models/emojiNamePict';
+import { EmojiService } from 'src/app/services/emoji.service';
 
 @Component({
   selector: 'app-team-emojis',
@@ -6,10 +8,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./team-emojis.component.css']
 })
 export class TeamEmojisComponent implements OnInit {
+  productDialog: boolean;
 
-  constructor() { }
+  products: EmojiNamePict[];
+
+  product: EmojiNamePict;
+
+  selectedProducts: EmojiNamePict[];
+
+  submitted: boolean;
+
+  constructor() {
+    this.products = [new EmojiNamePict(1, "blabla", "toto")];
+  }
 
   ngOnInit(): void {
+  }
+
+  openNew = () => {
+    this.product = new EmojiNamePict(null, null, null);
+    this.submitted = false;
+    this.productDialog = true;
+  }
+
+  hideDialog = () => {
+    this.productDialog = false;
+    this.submitted = false;
   }
 
 }
