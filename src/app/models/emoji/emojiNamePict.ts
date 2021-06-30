f500748828ac4910af6a24affc903c8250f181b7
Félix Burie <felbur012@gmail.com>
2021-07-01 00:37:11 +0200

    feat(emojis): display all default emojis

--- src/app/models/emoji/emojiNamePict.ts
+++ src/app/models/emoji/emojiNamePict.ts
@@ -1,28 +1,5 @@
-export class EmojiNamePict {
-
-    constructor(private _id: string, private _name: string, private _picture: string) { }
-
-    public get id(): string {
-        return this._id;
-    }
-
-    public set id(id: string) {
-        this._id = id;
-    }
-
-    public get name(): string {
-        return this._name;
-    }
-
-    public set name(name: string) {
-        this._name = name;
-    }
-
-    public get picture(): string {
-        return this._picture;
-    }
-
-    public set picture(picture: string) {
-        this._picture = picture;
-    }
+export interface EmojiNamePict {
+    id: string;
+    name: string;
+    picture: string;
 }
--- src/app/services/emoji.service.ts
+++ src/app/services/emoji.service.ts
@@ -11,14 +11,15 @@ import { EmojiNamePict } from '../models/emoji/emojiNamePict';
   providedIn: 'root'
 })
 export class EmojiService {
-
-  /** path to the folder of the emojis [default, organization, team] */
-  static path = ["emojis/", "emojisOrga/", "emojisTeams/"];
-
   url = environment.apiUrl + "/emojis/";
 
+  /** path to the folder of the emojis [team, organization, default] */
+  static paths = [/*"emojisTeams/",*/ "emojisOrga/", "emojis/"];
+
+  static _json: EmojiDesc[] = [];
+
   json: EmojiDesc[] = [];
-  nextEmojiId = 1_000_000;
+  nextEmojiId = 1_000_000; // give id to the emoji without id
   selectors: EmojisSelectorComponent[] = [];
 
   constructor(private http: HttpClient) {
@@ -29,6 +30,7 @@ export class EmojiService {
       this.json.sort((a, b) => a.order - b.order);
       this.json.forEach(item => item.annotation = item.annotation.substr(0, item.annotation.length - 2));
       this.json = this.json.filter(item => !item.annotation.includes("skin"))
+      EmojiService._json = this.json;
 
       this.selectors.forEach(select => select.emojis = this.json.slice(0, 50));
     });
@@ -118,20 +120,17 @@ export class EmojiService {
     return html + content.substring(prev);
   }
 
-  static getEmoji(name: string, teamId = "orga"): EmojiNamePict {
-    return
-  }
-
   /** Returns the matching emoji if it exits, undefined otherwise */
-  static _getEmoji = (name: string, teamId = "orga"): EmojiNamePict => {
-    let _emojis = [emojisBase, emojisOrga/*, emojisTeam[teamId]*/];
+  static getEmoji(name: string, teamId = "orga"): EmojiNamePict {
+    const emojis = [emojisOrga];
 
-    for (let index in _emojis) {
-      // TODO [Opti] -> search in dict
-      let emojis = Object.values(_emojis[index]).filter(emoji => emoji.name == name);
-      if (emojis.length != 0)
-        return new EmojiNamePict("" + emojis[0].id, emojis[0].name, EmojiService.path[index] + emojis[0].picture);
-    }
+    let emoji;
+    for (let index in emojis)
+      if (emoji = emojis[index].find(emoji => emoji.annotation == name))
+        return { id: undefined, name: name, picture: this.paths[index] + emoji.path };
+
+    if (EmojiService._json.find(emoji => emoji.annotation == name))
+      return { id: undefined, name: name, picture: "emojis/" + name };
     return undefined;
   }
 
@@ -150,256 +149,13 @@ export class EmojiService {
 // ================================================================================================
 
 /** Emojis added by the organization (available for all the teams) */
-let emojisOrga: { [id: number]: { id: number, name: string, picture: string } } = {
-  10_001: {
-    id: 10_001,
-    name: "m2i",
-    picture: "orga_1.png"
+let emojisOrga = [
+  {
+    annotation: "m2i",
+    path: "orga_1"
   },
-  10_002: {
-    id: 10_002,
-    name: "semifir",
-    picture: "orga_2.png"
-  },
-};
-
-// ================================================================================================
-
-/** Default emojis */
-let emojisBase: { [id: number]: { id: number, name: string, picture: string } } = {
-  1000: {
-    id: 1,
-    name: "middle_finger",
-    picture: "middle_finger.png"
-  },
-  1: {
-    id: 1,
-    name: "bagel",
-    picture: "bagel.png"
-  },
-  2: {
-    id: 2,
-    name: "beer_mug",
-    picture: "beer_mug.png"
-  },
-  3: {
-    id: 3,
-    name: "beverage_box",
-    picture: "beverage_box.png"
-  },
-  4: {
-    id: 4,
-    name: "birthday_cake",
-    picture: "birthday_cake.png"
-  },
-  5: {
-    id: 5,
-    name: "broccoli",
-    picture: "broccoli.png"
-  },
-  6: {
-    id: 6,
-    name: "bubble_tea",
-    picture: "bubble_tea.png"
-  },
-  7: {
-    id: 7,
-    name: "burrito",
-    picture: "burrito.png"
-  },
-  8: {
-    id: 8,
-    name: "cherries",
-    picture: "cherries.png"
-  },
-  9: {
-    id: 9,
-    name: "chocolate_bar",
-    picture: "chocolate_bar.png"
-  },
-  10: {
-    id: 10,
-    name: "clinking_beer_mugs",
-    picture: "clinking_beer_mugs.png"
-  },
-  11: {
-    id: 11,
-    name: "cocktail_glass",
-    picture: "cocktail_glass.png"
-  },
-  12: {
-    id: 12,
-    name: "cookie",
-    picture: "cookie.png"
-  },
-  13: {
-    id: 13,
-    name: "croissant",
-    picture: "croissant.png"
-  },
-  14: {
-    id: 14,
-    name: "cut_of_meat",
-    picture: "cut_of_meat.png"
-  },
-  15: {
-    id: 15,
-    name: "fire",
-    picture: "fire.png"
-  },
-  16: {
-    id: 16,
-    name: "flexed_biceps",
-    picture: "flexed_biceps.png"
-  },
-  17: {
-    id: 17,
-    name: "french_fries",
-    picture: "french_fries.png"
-  },
-  18: {
-    id: 18,
-    name: "frowning_face",
-    picture: "frowning_face.png"
-  },
-  19: {
-    id: 19,
-    name: "frowning_face_with_open_mouth",
-    picture: "frowning_face_with_open_mouth.png"
-  },
-  20: {
-    id: 20,
-    name: "grimacing_face",
-    picture: "grimacing_face.png"
-  },
-  21: {
-    id: 21,
-    name: "grinning_face_with_smiling_eyes",
-    picture: "grinning_face_with_smiling_eyes.png"
-  },
-  22: {
-    id: 22,
-    name: "grinning_face_with_sweat",
-    picture: "grinning_face_with_sweat.png"
-  },
-  23: {
-    id: 23,
-    name: "hamburger",
-    picture: "hamburger.png"
-  },
-  24: {
-    id: 24,
-    name: "hundred_points",
-    picture: "hundred_points.png"
-  },
-  25: {
-    id: 25,
-    name: "microbe",
-    picture: "microbe.png"
-  },
-  26: {
-    id: 26,
-    name: "middle_finger",
-    picture: "middle_finger.png"
-  },
-  27: {
-    id: 27,
-    name: "OK_hand",
-    picture: "OK_hand.png"
-  },
-  28: {
-    id: 28,
-    name: "pineapple",
-    picture: "pineapple.png"
-  },
-  29: {
-    id: 29,
-    name: "pizza",
-    picture: "pizza.png"
-  },
-  30: {
-    id: 30,
-    name: "red_apple",
-    picture: "red_apple.png"
-  },
-  31: {
-    id: 31,
-    name: "sandwich",
-    picture: "sandwich.png"
-  },
-  32: {
-    id: 32,
-    name: "shamrock",
-    picture: "shamrock.png"
-  },
-  33: {
-    id: 33,
-    name: "sleeping_face",
-    picture: "sleeping_face.png"
-  },
-  34: {
-    id: 34,
-    name: "slot_machine",
-    picture: "slot_machine.png"
-  },
-  35: {
-    id: 35,
-    name: "smiling_face_with_halo",
-    picture: "smiling_face_with_halo.png"
-  },
-  36: {
-    id: 36,
-    name: "spaghetti",
-    picture: "spaghetti.png"
-  },
-  37: {
-    id: 37,
-    name: "stop_sign",
-    picture: "stop_sign.png"
-  },
-  38: {
-    id: 38,
-    name: "tangerine",
-    picture: "tangerine.png"
-  },
-  39: {
-    id: 39,
-    name: "tomato",
-    picture: "tomato.png"
-  },
-  40: {
-    id: 40,
-    name: "tropical_drink",
-    picture: "tropical_drink.png"
-  },
-  41: {
-    id: 41,
-    name: "upside-down_face",
-    picture: "upside-down_face.png"
-  },
-  42: {
-    id: 42,
-    name: "victory_hand",
-    picture: "victory_hand.png"
-  },
-  43: {
-    id: 43,
-    name: "vulcan_salute",
-    picture: "vulcan_salute.png"
-  },
-  44: {
-    id: 44,
-    name: "watermelon",
-    picture: "watermelon.png"
-  },
-  45: {
-    id: 45,
-    name: "wine_glass",
-    picture: "wine_glass.png"
-  },
-  46: {
-    id: 46,
-    name: "zipper-mouth_face",
-    picture: "zipper-mouth_face.png"
+  {
+    annotation: "semifir",
+    path: "orga_2"
   }
-};
+];
